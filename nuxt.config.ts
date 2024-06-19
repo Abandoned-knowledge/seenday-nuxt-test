e88238a06a9a516fe1a6b8d09546699b094db1b7
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-06-19 15:13:40 +0300

    add font montserrat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,48 @@
 export default defineNuxtConfig({
   ssr: true,
+
   modules: [
     "@nuxtjs/tailwindcss",
     "@vee-validate/nuxt",
     "radix-vue/nuxt",
     "@nuxtjs/i18n",
   ],
+
   i18n: {
     strategy: "prefix_except_default",
     langDir: "./app/locales/",
     defaultLocale: "ru",
-    locales: [
-      {code: "ru", iso: "ru-RU", file: "ru-RU.json"}
-    ],
+    locales: [{ code: "ru", iso: "ru-RU", file: "ru-RU.json" }],
   },
+
   srcDir: "./src",
+
   components: {
     dirs: ["./components/"],
   },
+
   dir: {
     pages: "./pages/",
     layouts: "./app/layouts/",
     assets: "./app/assets/",
   },
+
   app: {
     head: {
       title: "Seenday",
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
+          crossorigin: "",
+        },
+      ],
     },
   },
 
