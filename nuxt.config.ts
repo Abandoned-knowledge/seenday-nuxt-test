6967f7b96bf4ff13c5311a5d8c63ae414ea4016d
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-06-19 15:01:22 +0300

    create base structure and configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  ssr: true,
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vee-validate/nuxt",
+    "radix-vue/nuxt",
+    "@nuxtjs/i18n",
+  ],
+  i18n: {
+    strategy: "prefix_except_default",
+    langDir: "./app/locales/",
+    defaultLocale: "ru",
+    locales: [
+      {code: "ru", iso: "ru-RU", file: "ru-RU.json"}
+    ],
+  },
+  srcDir: "./src",
+  components: {
+    dirs: ["./components/"],
+  },
+  dir: {
+    pages: "./pages/",
+    layouts: "./app/layouts/",
+    assets: "./app/assets/",
+  },
+  app: {
+    head: {
+      title: "Seenday",
+    },
+  },
+
+  runtimeConfig: {},
+});
