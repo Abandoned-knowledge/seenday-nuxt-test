818b05aa761b9a086ca94ddd18ce8cded0fcf0bc
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-06-20 14:55:35 +0300

    create shared ButtonSmall.vue

---
+++ src/shared/buttons/ButtonSmallGradient.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="body-text p-3 text-white rounded main-gradient-hover">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/shared/buttons/ButtonSmallSolid.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="body-text p-3 text-black rounded bg-lightGrey">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="scss" scoped></style>
