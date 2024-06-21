28b04f18dd710360cff60fd30dbab3085f23c836
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-06-21 16:01:05 +0300

    change font-size

--- src/components/Hero/HeroSection.vue
+++ src/components/Hero/HeroSection.vue
@@ -7,12 +7,12 @@ import Statictics from './Statictics.vue';
     <BGCurves class="absolute -z-10 mx-auto top-[10%] h-[900px]"/>
 
     <div class="container relative mt-10 sm:mt-32">
-        <section class="flex flex-col items-center sm:items-start text-center sm:text-start gap-3 w-full sm:w-[700px]">
+        <section class="flex flex-col items-center sm:items-start text-center sm:text-start gap-3 w-full sm:w-[550px] md:w-[450px] lg:w-[700px]">
             <h1 class="h1-text uppercase">{{ $t('heroSection.title') }}</h1>
         
             <p class="subtitle-text h-fit"> <span class="text-main font-bold">Seenday</span> {{ $t('heroSection.subtitle') }} </p>
 
-            <div class="relative sm:absolute h-fit w-[50%] top-[-15%] right-0">
+            <div class="relative sm:absolute h-fit w-[50%] top-[-15%] right-[-5%]">
                 <img src="@/app/img/helper_main.png" class="z-10"/>
                 <img src="@/app/img/helper_ellipse.png" class="hidden lg:absolute bottom-0 scale-110 z-[-1]"/>
             </div>
